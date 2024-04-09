8b0651ee2988c120ee00948cb86a68b44a72031f
Veronica Moss <vivettemoss@yahoo.com>
2024-04-09 17:45:09 -0500

    added linkedin to footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,4 +1,6 @@
 import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
 import "./Footer.css";
 import { Link } from "react-router-dom";
 
@@ -6,9 +8,14 @@ export default function Footer() {
   return (
     <div id="footer">
       <p id="about-me">Find Me</p>
-      <Link id="github-link" to="https://github.com/vmoss1">
-        <FaGithub /> vmoss1
-      </Link>
+      <div id="footer-icons" style={{ fontSize: "35px", color: "black" }}>
+        <Link id="github-link" to="https://github.com/vmoss1">
+          <FaGithub />
+        </Link>
+        <Link id="github-link" to="https://www.linkedin.com/in/veronica-moss/">
+          <FaLinkedin />
+        </Link>
+      </div>
     </div>
   );
 }
